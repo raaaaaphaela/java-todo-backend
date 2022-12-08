d7b362a883669ee6569a1a3e1cb0a1bd852d5449
Raphaela <raphaela.dohrmann@gmail.com>
2022-12-08 19:12:59 +0100

    Add DirtiesContext annotation

--- src/test/java/de/neuefische/backend/controller/ToDoControllerTest.java
+++ src/test/java/de/neuefische/backend/controller/ToDoControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -53,6 +54,7 @@ class ToDoControllerTest {
     }
 
     @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
     void putTodo_ShouldReturnOneUpdatedTodo() throws Exception {
         // given
         String initial = """
