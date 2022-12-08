621bfe01d8c9fd4e238f218cb3b114024d8fac56
Raphaela <raphaela.dohrmann@gmail.com>
2022-12-08 18:47:18 +0100

    Add two more integration tests

--- src/test/java/de/neuefische/backend/controller/ToDoControllerTest.java
+++ src/test/java/de/neuefische/backend/controller/ToDoControllerTest.java
@@ -19,7 +19,7 @@ class ToDoControllerTest {
     private MockMvc mvc;
 
     @Test
-    void getAllTodos_expectEmptyList() throws Exception {
+    void getAllTodos_InitialState_ShouldReturnEmptyList() throws Exception {
         // given
         String expectedJSON = "[]";
 
@@ -30,7 +30,7 @@ class ToDoControllerTest {
     }
 
     @Test
-    void postOneTodos_ShouldReturnOneTodo() throws Exception {
+    void postTodo_ShouldReturnOneTodo() throws Exception {
         // given
         String JSON = """
                 {
@@ -50,5 +50,72 @@ class ToDoControllerTest {
         mvc.perform(MockMvcRequestBuilders.get("/api/todo/0"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(JSON, true));
+    }
+
+    @Test
+    void putTodo_ShouldReturnOneUpdatedTodo() throws Exception {
+        // given
+        String initial = """
+                {
+                "description": "Neu",
+                "status": "OPEN"
+                }
+                """;
+
+        String saved = """
+                {
+                "id" : "0",
+                "description": "Neu",
+                "status": "OPEN"
+                }
+                """;
+
+        String updated = """
+                {
+                "id" : "0",
+                "description": "Todo: Changed",
+                "status": "OPEN"
+                }
+                """;
+
+        mvc.perform(MockMvcRequestBuilders.post("/api/todo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(initial))
+                .andExpect(status().isOk())
+                .andExpect(content().json(saved, true));
+
+        mvc.perform(MockMvcRequestBuilders.put("/api/todo/0")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updated))
+                .andExpect(status().isOk())
+                .andExpect(content().json(updated, true));
+    }
+
+    @Test
+    void deleteTodo_ShouldReturn200Response() throws Exception {
+        // given
+        String initial = """
+                {
+                "description": "Neu",
+                "status": "OPEN"
+                }
+                """;
+
+        String saved = """
+                {
+                "id" : "0",
+                "description": "Neu",
+                "status": "OPEN"
+                }
+                """;
+
+        mvc.perform(MockMvcRequestBuilders.post("/api/todo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(initial))
+                .andExpect(status().isOk())
+                .andExpect(content().json(saved, true));
+
+        mvc.perform(MockMvcRequestBuilders.delete("/api/todo/0"))
+                .andExpect(status().isOk());
     }
 }
